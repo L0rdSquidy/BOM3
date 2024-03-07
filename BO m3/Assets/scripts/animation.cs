5a7e14b823af0b8e02743070dcdaef2942d2a452
Senna van Santen <37044@ma-web.nl>
2024-03-07 09:45:34 +0100

    add

---
+++ BO m3/Assets/scripts/animation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Maak een variabele aan voor je animator component
+private Animator ani;
+
+void Start()
+{
+//Pak het animator component en sla die op in de variabele
+    ani = GetComponent<Animator>();
+}
+void Update()
+{
+//Check voor verticale input
+    if (Input.GetAxis("Vertical") > 0)
+    {
+//is de waarde groter dan 0 dan heb je een knop naar boven ingedrukt
+//Roep de juiste trigger aan!
+        ani.SetTrigger("run");
+//SetTrigger is trigger activeren
+        ani.ResetTrigger("Run exit");
+        ani.ResetTrigger("RunR");
+//ResetTrigger is Trigger de-activeren
+    }
+    else if (Input.GetAxis("Vertical") < 0)
+    {
+//is de waarde kleiner dan 0 dan heb je een knop naar beneden ingedrukt
+//Roep de juiste trigger aan
+        ani.SetTrigger("RunR");
+        ani.ResetTrigger("Run exit");
+        ani.ResetTrigger("run");
+    }
+    else {
+//is de waarde 0 dan heb je niets ingedrukt
+//Roep de juiste trigger aan
+        ani.SetTrigger("Run exit");
+        ani.ResetTrigger("run");
+        ani.ResetTrigger("RunR");
+    }
+}
+}
--- BO m3/Assets/scripts/animationtrigger.cs
+++ BO m3/Assets/scripts/animationtrigger.cs
@@ -14,33 +14,62 @@ public class animationtrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.T))
         {
             
             m_Animator.ResetTrigger("pushup");
 
+            m_Animator.ResetTrigger("run-jump");
+
             m_Animator.ResetTrigger("pray");
+
+            m_Animator.ResetTrigger("run");
             
             m_Animator.SetTrigger("jump");
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.R))
         {
             
             m_Animator.ResetTrigger("jump");
 
+            m_Animator.ResetTrigger("run-jump");
+
             m_Animator.ResetTrigger("pray");
+
+            m_Animator.ResetTrigger("run");
             
             m_Animator.SetTrigger("pushup");
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.E))
+        {
+            
+            m_Animator.ResetTrigger("jump");
+
+            m_Animator.ResetTrigger("run-jump");
+
+            m_Animator.ResetTrigger("pushup");
+
+            m_Animator.ResetTrigger("run");
+            
+            m_Animator.SetTrigger("pray");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
         {
             
             m_Animator.ResetTrigger("jump");
 
             m_Animator.ResetTrigger("pushup");
+
+            m_Animator.ResetTrigger("pray");
             
-            m_Animator.SetTrigger("pray");
+            m_Animator.SetTrigger("run-jump");
         }
+
+
+       
+
+        
     }
 }
